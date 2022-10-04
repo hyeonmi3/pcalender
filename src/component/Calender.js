70e46e15b8c1ba780e6b92033fc25c8fe40354cf
hyeonmi3 <cc146@daum.net>
2022-10-04 20:38:19 +0900

    calender

--- src/component/Calender.js
+++ src/component/Calender.js
@@ -1,7 +1,30 @@
-function Calender() {
+import '../style/calender.css'
+import { useState } from 'react';
+
+// moment 라이브러리 사용
+import moment from 'moment';
+
+const Calender = () => {
+  const [getMoment, setMoment] = useState(moment());
+
+  const today = getMoment;
+
+  const calendarArr = () => {
+    
+  }
+
   return (
     <div className="Calender">
-      {/* 프로젝트 실습 :P Calender */}
+      <div className="control">
+        <button onClick={() => { setMoment(getMoment.clone().subtract(1, 'month')) }} >이전달</button>
+        <span>{today.format('YYYY 년 MM 월')}</span>
+        <button onClick={() => { setMoment(getMoment.clone().add(1, 'month')) }} >다음달</button>
+      </div>
+      <table>
+        <tbody>
+          {calendarArr()}
+        </tbody>
+      </table>
     </div>
   );
 }
