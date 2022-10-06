db7864978c23afbe8e4f590ce6d99a75a3587066
hyeonmi3 <cc146@daum.net>
2022-10-06 20:48:50 +0900

    메뉴 추가

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,6 +2,7 @@ import '../style/header.css';
 
 function Header() {
   return (
+    // 1100 이상이면 헤더 이상
     <div className="Header">
       <header className='header'>
         <h2 className='logo'>:P Calender</h2>
@@ -9,6 +10,7 @@ function Header() {
           <span>LogIn</span>
           <span>SignUp</span>
         </nav>
+        <img className='menu' src='Img/menu.png'/>
       </header>
     </div>
   );
