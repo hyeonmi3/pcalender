fdef5852babbbc656f5a0485dfb246e3ab92ef11
hyeonmi3 <cc146@daum.net>
2022-11-01 17:26:09 +0900

    수정

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,11 +5,16 @@ function Header() {
     // 1100 이상이면 헤더 이상
     <div className="Header">
       <header className='header'>
+        {/* 로고 */}
         <h2 className='logo'>:P Calender</h2>
+        
+        {/* 로그인, 회원가입 */}
         <nav>
           <span>LogIn</span>
           <span>SignUp</span>
         </nav>
+
+        {/* 메뉴 */}
         <img className='menu' src='Img/menu.png'/>
       </header>
     </div>
