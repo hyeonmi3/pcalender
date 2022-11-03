5d597229be9f6d8b82727806e63eeec396d325b1
hyeonmi3 <cc146@daum.net>
2022-11-03 17:00:53 +0900

    calender

--- src/component/Login.js
+++ src/component/Login.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import '../style/login.css';
 
 function Login() {
+  
+
   return (
     <div className="Login">
       <div className='login'>
@@ -13,11 +15,11 @@ function Login() {
           {/* 입력 */}
           <div className='id_input'>
             <label>Id</label>
-            <input />
+            <input id="id" name="id" placeholder="아이디를 입력해주세요" />
           </div>
           <div className='password_input'>
             <label>password</label>
-            <input />
+            <input id="password" name="password" type="password" />
           </div>
 
           {/* 로그인 버튼 */}
