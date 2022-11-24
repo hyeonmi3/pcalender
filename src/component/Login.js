d6e9851376f45a4d308450085537cc49c55508ab
hyeonmi3 <cc146@daum.net>
2022-11-24 16:01:06 +0900

    로그인

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import '../style/auth.css';
 
 function Login() {
-  
-
   return (
     <div className="Login">
       <div className='login'>
@@ -13,16 +13,20 @@ function Login() {
 
           {/* 입력 */}
           <div className='id_input'>
-            <label>Id</label>
-            <input id="id" name="id" placeholder="아이디를 입력해주세요" />
+            <label htmlFor='input_id'>Id</label>
+            <input type='text' name="input_id" placeholder="id" />
           </div>
           <div className='password_input'>
-            <label>password</label>
-            <input id="password" name="password" type="password" placeholder='비밀번호를 입력해주세요' />
+            <label htmlFor='input_pw'>password</label>
+            <input type='password' name="input_pw" placeholder='password' />
           </div>
 
           {/* 로그인 버튼 */}
-          <button>LogIn</button>
+          <button onClick={
+            axios.get('http://localhost:3000/Calender')
+            .then((result)=>{})
+            .catch(()=>{})
+          }>LogIn</button>
         </div>
       </div>
     </div>
