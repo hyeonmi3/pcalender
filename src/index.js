39d4e57a43abf9d536b7f3344998c764768be33b
hyeonmi3 <cc146@daum.net>
2022-11-24 19:06:55 +0900

    로그인, 회원가입 삭제

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './style/all.css';
 
 import Calender from './component/Calender';
-import Login from './component/Login';
-import Signup from './component/Signup';
 import Header from './component/Header';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
-    <Login />
+    <Calender />
   </React.StrictMode>
 );
 
